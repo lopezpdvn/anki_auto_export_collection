c69467173122f73f3d232e386a854f781cd1a41e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-12-26 03:35:12 -0600

    hooks works

--- auto_export_collection/__init__.py
+++ auto_export_collection/__init__.py
@@ -1,4 +1,9 @@
-from anki import hooks
+from aqt import gui_hooks
 
 def log(msg):
     print('[{0}]: {1}'.format(__name__, msg))
+
+def f():
+    log('logged by addon after backup')
+
+gui_hooks.backup_did_complete.append(f)
