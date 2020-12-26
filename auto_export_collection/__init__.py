bcd54492b10faab76104dbc4eadfafe193bd7a89
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-12-26 03:56:33 -0600

    this works but throws error that collection is corrupted, which it is not

--- auto_export_collection/__init__.py
+++ auto_export_collection/__init__.py
@@ -1,9 +1,17 @@
-from aqt import gui_hooks
+from anki.exporting import AnkiCollectionPackageExporter
+from aqt import gui_hooks, mw
 
 def log(msg):
     print('[{0}]: {1}'.format(__name__, msg))
 
 def f():
+    fpath = r'C:\pathh\a.cokpkg'
+
+    exporter = AnkiCollectionPackageExporter(mw.col)
+    exporter.includeMedia = False
+    exporter.exportInto(fpath)
+
     log('logged by addon after backup')
 
-gui_hooks.backup_did_complete.append(f)
+#gui_hooks.backup_did_complete.append(f)
+gui_hooks.profile_will_close.append(f)
