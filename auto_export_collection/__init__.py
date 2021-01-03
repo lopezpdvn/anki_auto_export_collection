37e1267d3f5c49eb87e592aa53a97c4e553a7f15
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-01-03 02:50:18 -0600

    use config

--- auto_export_collection/__init__.py
+++ auto_export_collection/__init__.py
@@ -1,11 +1,18 @@
+from os.path import join, normpath, dirname
 from anki.exporting import AnkiCollectionPackageExporter
 from aqt import gui_hooks, mw
+
+def get_export_filepath():
+    config = mw.addonManager.getConfig(__name__)
+    path_suffix = normpath(config['export_filepath_rel_to_collection_dir'])
+    return join(dirname(mw.col.path), path_suffix)
 
 def log(msg):
     print('[{0}]: {1}'.format(__name__, msg))
 
 def f():
-    fpath = r'C:\pathh\a.cokpkg'
+    fpath = get_export_filepath()
+    log('Exporting to `{0}`...'.format(fpath))
 
     exporter = AnkiCollectionPackageExporter(mw.col)
     exporter.includeMedia = False
